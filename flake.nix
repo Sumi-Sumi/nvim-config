e35b4e53325a95f4b1c02e436324340c13334fe4
Sumi-Sumi <dragon511southern@gmail.com>
2023-01-11 23:57:18 +0900

    fix flake.nix

--- flake.nix
+++ flake.nix
@@ -4,21 +4,24 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    flakes = {
+      url = "github:Sumi-Sumi/flakes";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, flakes }:
+  outputs = inputs @ { self, nixpkgs, flake-utils, flakes }:
     let
       lib = nixpkgs.lib;
 
       ls = path: builtins.readDir path;
-      isModules = file: with lib; if (hasSuffix ".nix" file) then strings.removeSuffix ".nix" file else false;
-      modules = with builtins; lib.remove false (map isModules (attrNames (ls ./hm-module)));
+      isModules = file: with lib; if (hasSuffix ".nix" file) then strings.removeSuffix ".nix" file else null;
+      modules = with builtins; lib.remove null (map isModules (attrNames (ls ./hm-modules)));
       genModule = name: {
         inherit name;
-        value = import ./hm-module/${name}.nix;
+        value = import ./hm-modules/${name}.nix;
       };
       mkModules = with builtins; listToAttrs (map genModule modules);
-
     in
     {
       nixosModules = mkModules;
